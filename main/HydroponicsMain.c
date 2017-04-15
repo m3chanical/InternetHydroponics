2dd2cf7249057052b58a59f681300475f7f9d60e
carl anderson <canderson@bulogics.com>
2017-04-15 14:46:03 -0400

    refactor the main module a bit

--- main/HydroponicsMain.c
+++ main/HydroponicsMain.c
@@ -8,17 +8,48 @@
 #include "nvs_flash.h"
 #include "driver/gpio.h"
 
+#include "HydroponicsMain.h"
 #include "WifiInfo.h"
 #include "HydroponicsUtils.h"
 
 #include "mqtt.h"
 
-esp_err_t event_handler(void *ctx, system_event_t *event)
-{
-    return ESP_OK; // TODO: Make this more good-er.
-}
+// Private Function Declarations
+void hydroponicsInit(void);
+void wifiInit(void);
+esp_err_t event_handler(void *, system_event_t *);
+// Private Variables
+tcpip_adapter_ip_info_t ip;
+uint8_t level = 0;
 
 void app_main(void)
+{
+    // Initialize the things!
+    wifiInit();
+    hydroponicsInit();
+    
+    while (true) {
+        vTaskDelay(500 / portTICK_PERIOD_MS);
+        gpio_set_level(GPIO_NUM_4, level);
+        level = !level;
+        ESP_LOGI(LOG_TAG, "LED set to: [%d]", level);
+        if (tcpip_adapter_get_ip_info(ESP_IF_WIFI_STA, &ip) == 0) 
+        {
+            ESP_LOGI(LOG_TAG, "~~~~~~~~~~~");
+            ESP_LOGI(LOG_TAG, "IP:"IPSTR, IP2STR(&ip.ip));
+            ESP_LOGI(LOG_TAG, "MASK:"IPSTR, IP2STR(&ip.netmask));
+            ESP_LOGI(LOG_TAG, "GW:"IPSTR, IP2STR(&ip.gw));
+            ESP_LOGI(LOG_TAG, "~~~~~~~~~~~");
+        }
+    }
+}
+
+void hydroponicsInit(void)
+{
+    gpio_set_direction(GPIO_NUM_4, GPIO_MODE_OUTPUT);
+}
+
+void wifiInit(void)
 {
     nvs_flash_init();
     tcpip_adapter_init();
@@ -37,24 +68,10 @@ void app_main(void)
     ESP_ERROR_CHECK( esp_wifi_set_config(WIFI_IF_STA, &sta_config) );
     ESP_ERROR_CHECK( esp_wifi_start() );
     ESP_ERROR_CHECK( esp_wifi_connect() );
-
-    gpio_set_direction(GPIO_NUM_4, GPIO_MODE_OUTPUT);
-    int level = 0;
-    tcpip_adapter_ip_info_t ip;
     memset(&ip, 0, sizeof(tcpip_adapter_ip_info_t));
-    while (true) {
-        vTaskDelay(500 / portTICK_PERIOD_MS);
-        gpio_set_level(GPIO_NUM_4, level);
-        level = !level;
-        ESP_LOGI(LOG_TAG, "LED set to: [%d]", level);
-        if (tcpip_adapter_get_ip_info(ESP_IF_WIFI_STA, &ip) == 0) 
-        {
-            ESP_LOGI(LOG_TAG, "~~~~~~~~~~~");
-            ESP_LOGI(LOG_TAG, "IP:"IPSTR, IP2STR(&ip.ip));
-            ESP_LOGI(LOG_TAG, "MASK:"IPSTR, IP2STR(&ip.netmask));
-            ESP_LOGI(LOG_TAG, "GW:"IPSTR, IP2STR(&ip.gw));
-            ESP_LOGI(LOG_TAG, "~~~~~~~~~~~");
-        }
-    }
 }
 
+esp_err_t event_handler(void *ctx, system_event_t *event)
+{
+    return ESP_OK; // TODO: Make this more good-er.
+}
